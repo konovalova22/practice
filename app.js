8989bfab5bd600915f5f2f37ab1c8db2edfa65e6
Katya <konovalova.ep@prg-it.ru>
2021-09-23 16:04:21 +0500

    kk

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const columnDefs = [
-    { field: "make", editable: true },
-    { field: "model", editable: true },
-    { field: "price", editable: true }
+    { field: "income", editable: true },//доход
+    { field: "consumption", editable: true },//расход
+    { field: "goal", editable: true },//цель
+    { field: "spending", editable: true }//траты( сколько потратил за сегодня)
   ];
   
   // specify the data
   const rowData = [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 }
+    { income: 30000, consumption: 1000, goal: 10000, spending: 0},
+/*     { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 } */
   ];
   
   // let the grid know which columns and what data to use
@@ -32,7 +33,7 @@ const columnDefs = [
   function setSum() {
       let sum = 0;
       grid.gridOptions.api.forEachNode(node => {
-        sum += +node.data.price
+        sum += (+node.data.income - +node.data.consumption - +node.data.goal - +node.data.spending)/31
       });
       document.getElementById('sum').innerText = sum;
   }
